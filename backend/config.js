540d7f991029834f960cc3bce362a08bb5cc5c2b
Avishka-Weeraddana <weeraddanaar.21@uom.lk>
2024-09-02 19:06:58 +0530

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://avishka1999r:avishka99@cluster0.8nlep.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0"
+
---
+++ backend/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
+const app = express();
+
+//Middleware for pasing request body
+app.use(express.json());
+
+app.use(cors());
+//Middleware for cors
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Hello World");
+});
+
+app.use("/books", booksRoute);
+
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running on port : ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
